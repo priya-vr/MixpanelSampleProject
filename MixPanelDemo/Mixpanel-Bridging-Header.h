05c258c30e4458fd8a9c6f4d7808ac1ce3430de7
Priya VR <priya.vr@experionglobal.com>
2016-04-21 17:59:45 +0530

    first commit

---
+++ MixPanelDemo/Mixpanel-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Mixpanel-Bridging-Header.h
+//  MixPanelDemo
+//
+//  Created by Veena Nair on 21/04/16.
+//  Copyright © 2016 Veena Nair. All rights reserved.
+//
+
+#ifndef Mixpanel_Bridging_Header_h
+#define Mixpanel_Bridging_Header_h
+
+
+#endif /* Mixpanel_Bridging_Header_h */
